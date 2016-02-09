31558f23a364aa823e3a1cc9ee9f6e7d3be02301
Eben <ebenewood@yahoo.com>
2016-02-08 20:14:45 -0800

    includes js and html to allow for branching logic

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(function() {
+  if (confirm("If you are a Cyborg click Ok.")) {
+      $('#cyborg').show();
+    } else {
+      $('#human').show();
+    }
+});
+//   if (what = "cyborg") {
+//     $('#cyborg').show();
+//   } else if (what = "human") {
+//     $('#human').show();
+//   } else {
+//     $(function() {
+//       prompt("What are you. Seriously.");
+//   }
+// });
