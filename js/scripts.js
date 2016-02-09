c71f237decd9bb4a7786be509816ecfe9f1e8f88
Eben <ebenewood@yahoo.com>
2016-02-08 21:50:57 -0800

    creates looping

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,35 @@
+// $(function() {
+//   if (confirm("If you are a Cyborg click Ok.")) {
+//       $('#cyborg').show();
+//       var cyborg = true;
+//     } else {
+//       $('cyborg-text').append();
+//     }
+// });
+//
+// $(function() {
+//   if (cyborg) {
+//     $('#cyborg').hide();
+//   } else {
+//     $('#cyborg').show();
+//   }
+// });
+// $(function() {
+//   if (confirm()) {
+//     t = true;
+//   }
+//   else {
+//     t = false;
+//   }
+// });
+
 $(function() {
-  if (confirm("If you are a Cyborg click Ok.")) {
-      $('#cyborg').show();
-    } else {
-      $('#human').show();
+  var n = 0;
+  for (n = 0; n < 100; n++) {
+      $('#page').append(0);
     }
 });
+
 //   if (what = "cyborg") {
 //     $('#cyborg').show();
 //   } else if (what = "human") {
